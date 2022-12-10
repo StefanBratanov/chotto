06ace5ce794648005b44f9465fc5a11476abe8ca
Stefan Bratanov <stefan.bratanov93@gmail.com>
2022-12-10 15:29:08 +0200

    nits

--- src/main/java/chotto/cli/PropertiesVersionProvider.java
+++ src/main/java/chotto/cli/PropertiesVersionProvider.java
@@ -6,14 +6,15 @@ import picocli.CommandLine.IVersionProvider;
 
 /**
  * {@link IVersionProvider} implementation that returns version information from a {@code
- * /version.txt} file in the classpath. Copied from
- * https://github.com/remkop/picocli/blob/main/picocli-examples/src/main/java/picocli/examples/VersionProviderDemo1.java
+ * /version.txt} file in the classpath. Copied from <a
+ * href="https://github.com/remkop/picocli/blob/main/picocli-examples/src/main/java/picocli/examples/VersionProviderDemo1.java">VersionProviderDemo1.java</a>
  */
 public class PropertiesVersionProvider implements IVersionProvider {
+
   public String[] getVersion() throws Exception {
     final URL url = getClass().getResource("/version.txt");
     if (url == null) {
-      return new String[] {"No version.txt file found in the classpath."};
+      throw new IllegalStateException("No version.txt file found in the classpath.");
     }
     final Properties properties = new Properties();
     properties.load(url.openStream());
